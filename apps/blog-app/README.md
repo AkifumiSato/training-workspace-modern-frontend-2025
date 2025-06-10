942d25e0b5a6da94a5c70c696442330316269f0f
akfm.sato <akifumi_sato@r.recruit.co.jp>
2025-06-10 14:51:22 +0900

    blog app init

--- apps/blog-app/README.md
+++ apps/blog-app/README.md
@@ -1,9 +1,15 @@
-# Learn Next
+# Blog App
 
 Next.jsの課題を実装するためのアプリケーションです。課題の詳細は講師より提示されます。
 
 ## Getting Started
 
 ```shell-session
+# /packages/ui
+$ pnpm build # or pnpm dev
+```
+
+```shell-session
+# /apps/blog-app
 $ pnpm dev
 ```
--- apps/learn-next/app/page.tsx
+++ apps/learn-next/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@repo/ui/button";
-
-export default function Home() {
+export default function Page() {
   return (
-    <div className="flex flex-col gap-4 p-4">
-      <h1 className="text-3xl font-bold">Hello World</h1>
-      <Button>Click me</Button>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
