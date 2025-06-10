c9d9f5f69c5d1156f80b59fc73e97b45371f1ab8
akfm.sato <akifumi_sato@r.recruit.co.jp>
2025-06-10 15:35:56 +0900

    feat: blogのデザイン修正

--- apps/blog-app/app/_containers/posts/container.tsx
+++ apps/blog-app/app/_containers/posts/container.tsx
@@ -1,14 +1,22 @@
+import { Button } from "@repo/ui/button";
+import Link from "next/link";
 import { getPosts } from "./fetcher";
 
 export async function Posts() {
   const posts = await getPosts();
 
   return (
-    <ul className="flex flex-col gap-4">
+    <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
       {posts.map((post) => (
-        <li key={post.id} className="border-b border-gray-200 p-4">
+        <li
+          key={post.id}
+          className="grid grid-rows-[auto_1fr_auto] gap-3 border-b border-gray-200 p-4"
+        >
           <h2 className="text-lg font-bold">{post.title}</h2>
           <p className="text-sm text-gray-500">{post.body}</p>
+          <Button asChild variant="outline">
+            <Link href={`/posts/${post.id}`}>View Post</Link>
+          </Button>
         </li>
       ))}
     </ul>
